74d55c3deb16cd1521d9ab68ae0e4637fe843908
KACofficial <perryvance00@gmail.com>
2024-10-26 14:20:51 -0500

    add 404 not found page, and better wording.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomeView,
-    meta: { title: "Home | Learn | KAC" },
+    meta: { title: "Learn | KillAllChickens" },
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+    meta: { title: "Page Not Found | KillAllChickens" },
+  }
 ];
 
 const router = createRouter({
